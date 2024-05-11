0675872a85e223e05c5682823eca4bb42e775f95
nwil850 <nwil850@aucklanduni.ac.nz>
2024-05-11 16:47:00 +1200

    Started project, imported s&p 500 data and formatted

---
+++ Main.py
@@ -0,0 +1,30 @@
+from statsmodels.regression.rolling import RollingOLS
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import pandas as pd
+import numpy as np
+import datetime as dt
+import yfinance as yf
+import pandas_ta
+import warnings
+warnings.filterwarnings('ignore')
+
+sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+
+sp500['Symbol'] = sp500['Symbol'].str.replace('.', '-')
+
+symbols_list = sp500['Symbol'].unique().tolist() #Not survivorship bias free
+
+end_date = '2024-05-11'
+
+start_date = pd.to_datetime(end_date)-pd.DateOffset(365*8)
+
+df = yf.download(tickers=symbols_list,
+                 start=start_date,
+                 end=end_date).stack()
+
+df.index.names = ['date', 'ticker']
+df.columns = df.columns.str.lower()
+
+print(df)
